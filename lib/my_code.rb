0e826c759b502cde0dee092d16f428ec9429c838
Placido Wang <placidow@gmail.com>
2020-01-22 12:36:06 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,65 @@
 # My Code here....
+def map_to_negativize(source_array)
+  negative_numbers = []
+  
+  for num in source_array do
+     negative_numbers << num * -1
+  end
+  
+  return negative_numbers
+end
+
+def map_to_no_change(source_array)
+  return source_array
+end
+
+def map_to_double(source_array)
+  doubled_numbers = []
+  
+  for number in source_array do
+    doubled_numbers << number * 2
+  end
+  
+  return doubled_numbers
+end
+
+def map_to_square(source_array)
+  squared_numbers = []
+  
+  for number in source_array do
+    squared_numbers << number ** 2
+  end
+  
+  return squared_numbers
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  total = starting_point
+  
+  for number in source_array do
+    total += number
+  end
+  
+  return total
+end
+
+def reduce_to_all_true(source_array)
+  all_true = true
+  
+  for element in source_array do
+    if !element
+      all_true = nil
+    end
+  end
+  
+  return all_true
+end
+
+def reduce_to_any_true(source_array)
+  for element in source_array do
+    if element
+      return true
+    end
+  end
+  return false
+end
